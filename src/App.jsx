13adb693a90ab3f29daea3c526914b4a91c8b71d
v24kuon <63438234+v24kuon@users.noreply.github.com>
2022-03-26 18:06:29 +0900

    コード改良版

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,50 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+//チェックボックスの初期値を設定
 const App = () => {
-  const [currentCheckboxId, setCheckboxId] = useState({
-    naming: false,
-    fullname: false,
-  });
+  const checkboxLength = 3;
+  const getInitialValues = () => Array(checkboxLength).fill(false);
+  const [checked, setChecked] = useState(getInitialValues());
 
+  //ローカルストレージに保存されているチェックボックスの結果を取得
   useEffect(() => {
-    const data = {
-      naming: localStorage.getItem('naming') === 'true' ? true : false,
-      fullname: localStorage.getItem('fullname') === 'true' ? true : false,
-    };
-    setCheckboxId(data);
+    const json1 = localStorage.getItem('checked');
+    if (json1) {
+      const array1 = JSON.parse(json1);
+      setChecked(array1);
+    }
   }, []);
 
-  const setCheckbox = (event) => {
-    const naming = event.target.checked;
-    localStorage.setItem('naming', naming);
-
-    setCheckboxId((prevData) => ({
-      ...prevData,
-      naming: naming,
-    }));
+  // チェックボックスの結果をローカルストレージに保存
+  const onChange = (e, i) => {
+    const array1 = checked.map((x2, i2) => (i2 === i ? e.target.checked : x2));
+    setChecked(array1);
+    const json1 = JSON.stringify(array1);
+    localStorage.setItem('checked', json1);
   };
 
-  const setCheckbox2 = (event) => {
-    const fullname = event.target.checked;
-    localStorage.setItem('fullname', fullname);
+  // 複数のチェックボックス
+  const checkboxes = checked.map((x, i) => (
+    <div>
+      <label>
+        チェックボックス{i + 1}
+        <input type='checkbox' checked={x} onChange={(e) => onChange(e, i)} />
+      </label>
+    </div>
+  ));
 
-    setCheckboxId((prevData) => ({
-      ...prevData,
-      fullname: fullname,
-    }));
+  //ローカルストレージの内容削除
+  const removeCheckbox = () => {
+    localStorage.clear();
+    setChecked(getInitialValues());
   };
 
   return (
     <div className='App'>
       <h1>リロードしても消えないチェックボックス</h1>
-      <label>
-        ニックネーム
-        <input type='checkbox' onChange={setCheckbox} id='first' checked={currentCheckboxId.naming} />
-      </label>
-
-      <label>
-        フルネーム
-        <input type='checkbox' onChange={setCheckbox2} id='second' checked={currentCheckboxId.fullname} />
-      </label>
+      {checkboxes}
+      <button onClick={removeCheckbox}>チェックを全て外す</button>
     </div>
   );
 };
